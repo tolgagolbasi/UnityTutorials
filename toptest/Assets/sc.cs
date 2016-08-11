937e345a9e10061383a48c5a1e503686496c144e
tolgagolbasi <tolgagolbasi@gmail.com>
2016-08-11 10:45:56 +0300

    Unity Tutorials

---
+++ toptest/Assets/sc.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sc : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 temp = transform.position;
+		float a = Screen.width / 2;
+		float b = Screen.height / 2;
+		temp.x = ((Input.mousePosition.x - a)/a)-5;
+		temp.y = ((Input.mousePosition.y - b)/b)+5;
+		transform.position = temp;
+		Debug.Log(Input.mousePosition.x);
+	}
+}
