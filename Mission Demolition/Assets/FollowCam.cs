57136b78786e0821c1a3fbd7f7de0d6759a4828a
tolgagolbasi <tolgagolbasi@gmail.com>
2016-08-17 20:51:01 +0300

    Mission Demolition

---
+++ Mission Demolition/Assets/FollowCam.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+	static public FollowCam S;
+	public float easing = 0.05f;
+	public Vector2 minXY;
+	public bool ________________________;
+	public GameObject poi;
+	public float camZ;
+
+	void Awake(){
+		S = this;
+		camZ = this.transform.position.z;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (poi == null) {
+			return;
+		}
+		Vector3 destination = poi.transform.position;
+		destination.x = Mathf.Max (minXY.x, destination.x);
+		destination.y = Mathf.Max (minXY.y, destination.y);
+		destination = Vector3.Lerp (transform.position, destination, easing);
+		destination.z = camZ;
+		transform.position = destination;
+		this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+	}
+}
